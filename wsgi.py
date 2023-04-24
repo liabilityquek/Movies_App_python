cc07d3f1a70ef46fba6c58fb50f38b3061281dd6
Harold <haroldquekhere@gmail.com>
2023-04-24 17:55:26 +0800

    added host into wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from app import app
 from dotenv import load_dotenv
+import os
 
-load_dotenv() 
+load_dotenv()
 
 if __name__ == "__main__":
-    app.run()
+    host = os.getenv('HOST', '0.0.0.0')
+    app.run(host=host)
